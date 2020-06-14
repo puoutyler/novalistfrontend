a3ea3af14def7683cad657bfcf4fd07c453d65a6
Zeverai <zeveraimusic@gmail.com>
2020-06-14 12:15:07 -0400

    Added .length test to API render ternary.

--- src/App/index.js
+++ src/App/index.js
@@ -113,7 +113,7 @@ const App = (props) => {
     <div className="Api_container">
       <div>
       <ul className="App_row">
-        {apiBooks ? apiBooks > 0 ? apiBooks.map((newBook, index) => {
+        {apiBooks ? apiBooks.length > 0 ? apiBooks.map((newBook, index) => {
           return (
                   <li key={index} className="API-li">
                     {/* <h1 className="card-title">{newBook.volumeInfo.title}</h1> */}
